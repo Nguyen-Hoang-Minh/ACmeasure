49bb1131f41dd15c427761dfec183710dbb7524f
Minh Nguyen <minh.nguyen2810@hcmut.edu.vn>
2024-08-17 13:44:56 +0700

    test for host local server

--- src/main.cpp
+++ src/main.cpp
@@ -8,6 +8,8 @@
 #include "task.h"
 #include <WiFi.h>
 #include "temp_humi_sensor.h"
+#include "Preferences.h"
+#include "ESPAsyncWebServer.h"
 // #include "AdafruitConnect.h"
 // put function declarations here:
 M5GFX display;
@@ -18,6 +20,12 @@ Ticker updateAC;
 Ticker tickercontrolRelay;
 Ticker tickerMQTTpublish;
 Ticker tickerTempHumi;
+
+AsyncWebServer server(80);
+Preferences preferences;
+
+String wf_ssid;
+String wf_pw;
 void setup() {
   // put your setup code here, to run once:
   auto cfg = M5.config();
@@ -26,49 +34,83 @@ void setup() {
   Wire.begin();
   //DHT.begin();
   M5Dial.Encoder.write(-1000);
-  setupPahub();
-  setupTempHumi();
-  switchChanel_2();
-  setupACsensor();
-  switchChanel_3();
-  setupRelay();
-  ALLrelayState=OFF_ALL;
-  controlALL();
-  WiFi.mode(WIFI_STA);
-  WiFi.begin(ssid, password);
-  setupMQTT();
-  init_screen();
-  while (WiFi.status() != WL_CONNECTED){
-    M5Dial.Lcd.setCursor(158, 106);
-    M5Dial.Lcd.setTextSize(1);
-    M5Dial.Lcd.printf("Connecting...");
-  }
-  while(WiFiConnectFlag!=true && MQTTConnectFlag!=true){
-    checkWiFiStatus();
-    checkConnectMQTT(); 
-  }
-  updateAC.attach_ms(211,taskUpdateACinfor);
-  tickerTempHumi.attach_ms(2002,getTempandHumi);
-  updatescreen.attach_ms(1023,taskUpdateScreen);
-  tickercontrolRelay.attach_ms(53,taskControlRelay);
-  tickerMQTTpublish.attach_ms(5001, taskClientPublish);
+  // setupPahub();
+  // setupTempHumi();
+  // switchChanel_2();
+  // setupACsensor();
+  // switchChanel_3();
+  // setupRelay();
+  // ALLrelayState=OFF_ALL;
+  // controlALL();
+  WiFi.mode(WIFI_AP);
+  WiFi.softAP("M5Dial_Config", "12345678");
+  server.begin();
+  // Set up the web server routes
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request) {
+    request->send(200, "text/html", "<form action='/get' method='post'><label>SSID:</label><input type='text' name='ssid'><br><label>Password:</label><input type='password' name='pass'><br><input type='submit' value='Save'></form>");
+  });
 
-  clientPublish("kienpham/feeds/topic0","0");
-  clientPublish("kienpham/feeds/topic1","0");
-  clientPublish("kienpham/feeds/topic2","0");
-  clientPublish("kienpham/feeds/topic3","0");
-  clientPublish("kienpham/feeds/topic4","0");
+    server.on("/get", HTTP_POST, [](AsyncWebServerRequest *request) {
+
+    if (request->hasParam("ssid", true)) {
+      wf_ssid = request->getParam("ssid", true)->value();
+    }
+    if (request->hasParam("pass", true)) {
+      wf_pw = request->getParam("pass", true)->value();
+    }
+    
+    // Store WiFi credentials in Preferences
+    // preferences.begin("WiFiCreds", false);
+    // preferences.putString("ssid", ssid);
+    // preferences.putString("pass", );
+    // preferences.end();
+    M5Dial.Lcd.setCursor(40,40);
+    M5Dial.Lcd.print(wf_ssid);
+    M5Dial.Lcd.setCursor(40,48);
+    M5Dial.Lcd.print(wf_pw);
+    String response = "Saved SSID: " + wf_ssid + "<br>Password: " + wf_pw;
+    request->send(200, "text/html", response + "<br><a href='/'>Go back</a>");
+  });
+  
+    while(wf_ssid==""){
+    }
+    WiFi.mode(WIFI_STA);
+    WiFi.begin(wf_ssid, wf_pw);
+  // setupMQTT();
+  // init_screen();
+  // while (WiFi.status() != WL_CONNECTED){
+  //   M5Dial.Lcd.setCursor(158, 106);
+  //   M5Dial.Lcd.setTextSize(1);
+  //   M5Dial.Lcd.printf("Connecting...");
+  // }
+  // while(WiFiConnectFlag!=true && MQTTConnectFlag!=true){
+  //   checkWiFiStatus();
+  //   checkConnectMQTT(); 
+  // }
+  // updateAC.attach_ms(211,taskUpdateACinfor);
+  // tickerTempHumi.attach_ms(2002,getTempandHumi);
+  // updatescreen.attach_ms(1023,taskUpdateScreen);
+  // tickercontrolRelay.attach_ms(53,taskControlRelay);
+  // tickerMQTTpublish.attach_ms(5001, taskClientPublish);
+
+  // clientPublish("kienpham/feeds/topic0","0");
+  // clientPublish("kienpham/feeds/topic1","0");
+  // clientPublish("kienpham/feeds/topic2","0");
+  // clientPublish("kienpham/feeds/topic3","0");
+  // clientPublish("kienpham/feeds/topic4","0");
 
 }
+
 void loop() {
   //put your main code here, to run repeatedly:
-    if (WiFi.status() == WL_CONNECTED)
-  {
-    checkWiFiStatus();
-    checkConnectMQTT();
-    clientLoop(); 
-  }
-  //  READ DATA
-  //Serial.print("DHT20, \t");
-  
+  //   if (WiFi.status() == WL_CONNECTED)
+  // {
+  //   checkWiFiStatus();
+  //   checkConnectMQTT();
+  //   clientLoop(); 
+  // }
+  if(WiFi.status() == WL_CONNECTED)
+    {
+      M5Dial.Speaker.tone(3000, 5000);
+    }
 }
